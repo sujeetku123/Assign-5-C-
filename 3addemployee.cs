b35978c79ce3624b51d7960cd98c7b332e11fbeb
sujeetku123 <102528893+sujeetku123@users.noreply.github.com>
2022-04-19 19:37:45 +0530

    Add files via upload

---
+++ 3addemployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    internal class addemployee
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            list.Add("ajay");
+            list.Add("akula");
+            list.Add("suri");
+            list.Add("amar");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("List of employees" + ':' + list[i]);
+            }
+            Console.WriteLine("total number of employess" + ':' + list.Count);
+            Console.ReadLine();
+        }
+    }
+}
